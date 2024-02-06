d2c2fd8fa21eb05ed06c8be261d720e586747daf
ColinAdo <colinadodev30@gmail.com>
2024-02-06 19:35:24 +0300

    Add URLConf for user profile

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from .views import UserView, CommentView, ProfileView, VoteList
 
 urlpatterns = [
     path('users/', UserView.as_view(), name='users'),
+    path('users/<int:pk>/profile/', ProfileView.as_view(), name='users'),
     path('comments/', CommentView.as_view(), name='comments'),
     path("comments/<int:pk>/votes/", VoteList.as_view(), name="vote_list"),
-    path('profiles/', ProfileView.as_view(), name='profiles'),
 ]
