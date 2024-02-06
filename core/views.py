649483ac55cd8a972d2465ddd38507258775cfd6
ColinAdo <colinadodev30@gmail.com>
2024-02-06 18:19:25 +0300

    Add views

--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 
-from .serializers import UserSerializer, CommentSerializer
-from .models import Comment
+from .serializers import UserSerializer, CommentSerializer, ProfileSerializer, VoteSerializer
+from .models import Comment, Profile, Vote
 
 class UserView(generics.ListCreateAPIView):
     queryset = get_user_model().objects.all()
@@ -11,3 +11,14 @@ class UserView(generics.ListCreateAPIView):
 class CommentView(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+class ProfileView(generics.ListCreateAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class VoteList(generics.ListCreateAPIView):
+    def get_queryset(self):
+        queryset = Vote.objects.filter(comment_id=self.kwargs["pk"])
+        return queryset
+    serializer_class = VoteSerializer
+
