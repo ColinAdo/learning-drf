bd239e06783f79dddda08307e5e033bc997d9f00
ColinAdo <colinadodev30@gmail.com>
2024-02-06 18:19:03 +0300

    Add serializers

--- core/serializers.py
+++ core/serializers.py
@@ -1,20 +1,34 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import Comment
+from .models import Comment, Profile, Vote
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
+    profile = ProfileSerializer(read_only=True, required=False)
     
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'email', 'age', )
+        fields = ('id', 'username', 'email', 'age', 'profile', )
 
     def validate_email(self, value):
         return value.lower()
     
+
+class VoteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Vote
+        fields = '__all__'
+
 class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    votes = VoteSerializer(many=True, read_only=True, required=False)
 
     class Meta:
         model = Comment
