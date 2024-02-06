7a7b8a79881d16161e5b66876c3df0f41222601d
ColinAdo <colinadodev30@gmail.com>
2024-02-06 18:17:58 +0300

    Add profile and vote models

--- core/models.py
+++ core/models.py
@@ -11,4 +11,16 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.content[:2]
+        return self.content[:20]
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,  related_name='profile', on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.bio[:10]
+    
+class Vote(models.Model):
+    comment = models.ForeignKey(Comment, related_name='votes', on_delete=models.CASCADE)
+    voted_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
