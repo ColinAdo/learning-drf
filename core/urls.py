bd6919481f3ac5a0d3e35b90982d13b367f4c0e9
ColinAdo <colinadodev30@gmail.com>
2024-02-06 21:46:55 +0300

    Add account URLConfs

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
 
-from .views import UserView, CommentView, ProfileView, VoteList
+from .views import (
+    UserView, 
+    UserDetailView, 
+    CommentView, 
+    ProfileView, 
+    VoteList, 
+    AccountView,
+    AccountDetailView
+)
 
 urlpatterns = [
     path('users/', UserView.as_view(), name='users'),
+    path('users/<int:pk>/', UserDetailView.as_view(), name='user-detail'),
     path('users/<int:pk>/profile/', ProfileView.as_view(), name='users'),
     path('comments/', CommentView.as_view(), name='comments'),
     path("comments/<int:pk>/votes/", VoteList.as_view(), name="vote_list"),
+    path('accounts/', AccountView.as_view(), name='accounts'),
+    path('accounts/<int:pk>/', AccountDetailView.as_view(), name='account-detail'),
+
 ]
