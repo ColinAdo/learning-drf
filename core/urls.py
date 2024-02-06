56ddaf87a0bfd0b85c0addd39455d978f21c24bb
ColinAdo <colinadodev30@gmail.com>
2024-02-06 18:19:48 +0300

    Add URLConfs

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import UserView, CommentView
+from .views import UserView, CommentView, ProfileView, VoteList
 
 urlpatterns = [
     path('users/', UserView.as_view(), name='users'),
     path('comments/', CommentView.as_view(), name='comments'),
+    path("comments/<int:pk>/votes/", VoteList.as_view(), name="vote_list"),
+    path('profiles/', ProfileView.as_view(), name='profiles'),
 ]
